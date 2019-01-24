512f473b5adf862013af56e38babc4bc74871574
Virgil <virgil@firstpromoter.com>
2019-01-24 14:39:36 +0200

    fix

--- lib/paymentrails.rb
+++ lib/paymentrails.rb
@@ -1,2 +1,7 @@
+require 'paymentrails/Recipient'
+require 'paymentrails/RecipientAccount'
+require 'paymentrails/Payment'
+require 'paymentrails/Batch'
+#require 'paymentrails/Balance'
 require 'paymentrails/Gateway'
 require 'paymentrails/Configuration'
